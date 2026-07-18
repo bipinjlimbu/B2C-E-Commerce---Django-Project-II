5afca564ab094cda745a80d8bcdb2dd5ddfa11fc
bipinjlimbu <bipinlimbu712@gmail.com>
2026-07-18 19:45:20 +0545

    Register Page Logic Added

--- cc_project/cc_app/views/auth_view.py
+++ cc_project/cc_app/views/auth_view.py
@@ -2,12 +2,63 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from ..models import User
 
 def register_view(request):
+    if request.user.is_authenticated:
+        messages.info(request, 'You are already logged in.')
+        return redirect('/')
+    
+    errors = {}
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        password = request.POST.get('password')
+        confirm_password = request.POST.get('confirm_password')
+        phone = request.POST.get('phone')
+        address = request.POST.get('address')
+        profile_picture = request.FILES.get('profile_picture')
+
+        if not username:
+            errors['username'] = 'Username is required.'
+        elif User.objects.filter(username=username).exists():
+            errors['username'] = 'Username is already taken.'
+            
+        if not email:
+            errors['email'] = 'Email is required.'
+        elif User.objects.filter(email=email).exists():
+            errors['email'] = 'Email is already registered.'
+            
+        if not password:
+            errors['password'] = 'Password is required.'
+        if password != confirm_password:
+            errors['confirm_password'] = 'Passwords do not match.'
+            
+        if errors:
+            return render(request, 'auth/register_page.html', {'errors': errors, 'data': request.POST})
+        
+        user = User.objects.create_user(
+            username=username,
+            email=email,
+            first_name=first_name,
+            last_name=last_name,
+            password=password,
+            phone=phone,
+            address=address,
+            profile_picture=profile_picture
+        )
+        user.save()
+        
+        messages.success(request, 'You have successfully registered. Please log in.')
+        return redirect('/login/')
+    
     return render(request, 'auth/register_page.html')
 
 def login_view(request):
     if request.user.is_authenticated:
+        messages.info(request, 'You are already logged in.')
         return redirect('/')
     
     errors = {}
