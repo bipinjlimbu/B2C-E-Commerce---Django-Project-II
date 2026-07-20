c2e92311760c180e9fc53d6761ee243a87c34e28
bipinjlimbu <bipinlimbu712@gmail.com>
2026-07-20 15:52:20 +0545

    My Reviews Section Added

--- cc_project/cc_app/views/dashboard.py
+++ cc_project/cc_app/views/dashboard.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from ..models import User, Product, Order
+from ..models import User, Product, Order, Review
 from django.db import models
 
 @login_required
@@ -61,6 +61,6 @@ def customer_dashboard_view(request):
     elif section == 'total-spent':
         context['total_spent'] = Order.objects.filter(customer=request.user, status='completed')
     elif section == 'my-reviews':
-        context['my_reviews'] = None
+        context['my_reviews'] = Review.objects.filter(customer=request.user).order_by('-created_at')
         
     return render(request, 'dashboard/customer_dashboard.html', context)
